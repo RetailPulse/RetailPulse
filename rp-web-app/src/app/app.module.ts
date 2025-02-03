39d050e3c914be8edca7593a5228154f0af6964e
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2025-02-03 23:43:55 +0800

    feat: manage to input the side menu of the application

--- rp-web-app/src/app/app.module.ts
+++ rp-web-app/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
-import  {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {SidebarComponent} from './core/sidebar/sidebar.component';
 import {CoreModule} from './core/core.module';
 import {AppRoutingModule} from './app.routes';
 import {ProductManagementComponent} from './product-management/product-management.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgbModule, NgbOffcanvas, NgbOffcanvasModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,14 +16,12 @@ import {ProductManagementComponent} from './product-management/product-managemen
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
     CoreModule,
     AppRoutingModule,
     LoginFormComponent,
     ProductManagementComponent,
-    ProductManagementComponent
+    NgbModule,
+    NgbOffcanvasModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
