235a4a34b5034466d7a9ae22b3e97a56ffdca054
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2025-02-03 21:24:08 +0800

    fix: change the theme and background of the login form component

--- rp-web-app/src/app/app.module.ts
+++ rp-web-app/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import  {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SidebarComponent} from './core/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    SidebarComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,LoginFormComponent]
 })
 export class AppModule { }
