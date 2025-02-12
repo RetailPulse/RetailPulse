4ab66d8e71ffa5edcb521c5d0ede65c2a2577d30
William Siling Tjhi <e1503302@u.nus.edu>
2025-02-12 18:06:16 +0800

    SCRUM-56 complete refresh token feature

--- rp-web-app/src/app/app.component.ts
+++ rp-web-app/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
 
   isLoginPage = false
 
-  constructor(private router: Router) { //private authService: AuthService, 
+  constructor(private router: Router) { //private authService: AuthService,
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.isLoginPage = event.url === '/';
