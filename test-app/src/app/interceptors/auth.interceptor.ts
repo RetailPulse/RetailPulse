d83f1e309c9fe1274a3a17b6379461384373b78d
Casper Yap <casperyap@live.com.sg>
2025-02-13 21:27:35 +0800

    Merge with main branch. Refresh token and code refactoring done by William.

---
+++ test-app/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('AuthInterceptor: Intercepting request', req); 
+
+    return next.handle(req);
+  }
+}
