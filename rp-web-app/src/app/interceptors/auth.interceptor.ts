057d6d56f6d623a3d8f39782166c4698f088ddf6
William Siling Tjhi <e1503302@u.nus.edu>
2025-02-15 18:25:53 +0800

    fixing cors issue at backend

--- rp-web-app/src/app/interceptors/auth.interceptor.ts
+++ rp-web-app/src/app/interceptors/auth.interceptor.ts
@@ -1,15 +1,33 @@
-import { inject } from '@angular/core';
-import {HttpInterceptorFn } from '@angular/common/http';
-import { AuthService } from '../services/auth.service';
-
+import {inject} from '@angular/core';
+import {HttpInterceptorFn} from '@angular/common/http';
+import {AuthService} from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('Processing authInterceptor...');
+
+  const patterns = [
+    '/.well-known/openid-configuration',
+    '/oauth2/jwks',
+    '/oauth2/token',
+    '/login'
+  ];
+
   // Inject the AuthService
   const authService = inject(AuthService);
 
   // Get the token from the AuthService
   const token = authService.accessToken;
+
+  // Skip the interceptor for OPTIONS requests (Preflight request)
+  if (req.method === 'OPTIONS') {
+    return next(req); // Simply pass the OPTIONS request without modification
+  }
+
+  // Exclude the URL of the openid-configuration endpoint
+  const matches = patterns.some(pattern => req.url.includes(pattern));
+  if (matches) {
+    return next(req); // Skip adding the Authorization header for this request
+  }
 
   // Clone the request and add the authorization header if the token exists
   if (token) {
