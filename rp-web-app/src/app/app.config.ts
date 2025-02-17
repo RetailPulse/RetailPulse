89b1cdee7485ec077062b71db16107025d038401
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2025-02-17 22:18:53 +0800

    refactor: update textarea component and streamline app configuration

--- rp-web-app/src/app/app.config.ts
+++ rp-web-app/src/app/app.config.ts
@@ -1,16 +1,18 @@
 import { authInterceptor } from "./interceptors/auth.interceptor";
 
 import { OAuthModule } from 'angular-oauth2-oidc';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {HttpClientModule, provideHttpClient, withInterceptors} from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router'; //, withDebugTracing
 import { routes } from './app.routes';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), //, withDebugTracing()
+    provideAnimations(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom(OAuthModule.forRoot())
+    importProvidersFrom(OAuthModule.forRoot(),HttpClientModule)
   ]
 };
