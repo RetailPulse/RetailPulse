1b8d64e33f5ecd210037b675b76395088501f801
Casper Yap <casyap@gmail.com>
2025-02-20 21:00:24 +0800

    Convert AuthGuardService (class) to authGuard (function) to comply with new Angular convention.

--- rp-web-app/src/app/app.routes.ts
+++ rp-web-app/src/app/app.routes.ts
@@ -1,22 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { OperatorPageComponent } from './operator-page/operator-page.component';
-import { AuthGuardService } from './services/authguard.guard';
-import {ProductManagementComponent} from './product-management/product-management.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'admin', component: AdminPageComponent, canActivate: [AuthGuardService], data: { roles: ['ADMIN', 'SUPER'] } },
-  { path: 'operator', component: OperatorPageComponent, canActivate: [AuthGuardService], data: { roles: ['OPERATOR'] }  },
+  { path: 'admin', component: AdminPageComponent, canActivate: [authGuard], data: { roles: ['ADMIN', 'SUPER'] } },
+  { path: 'operator', component: OperatorPageComponent, canActivate: [authGuard], data: { roles: ['OPERATOR'] }  },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
