4c9422ee38dc1a238f3a17be6b38bec563906ba8
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2025-02-23 23:11:07 +0800

    fix: update app.config.ts

--- rp-web-app/src/app/app.config.ts
+++ rp-web-app/src/app/app.config.ts
@@ -1,19 +1,16 @@
 import { authInterceptor } from "./interceptors/auth.interceptor";
 
 import { OAuthModule } from 'angular-oauth2-oidc';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {HttpClientModule, provideHttpClient, withInterceptors} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router'; //, withDebugTracing
 import { routes } from './app.routes';
-import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), //, withDebugTracing()
-    provideAnimations(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom(OAuthModule.forRoot(),HttpClientModule)
+    importProvidersFrom(OAuthModule.forRoot())
   ]
 };
