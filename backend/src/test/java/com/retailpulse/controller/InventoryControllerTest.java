11705fe92a8f98b56d3a3a31c130abf371be0d7c
Casper Yap <casperyap@live.com.sg>
2025-03-15 00:37:22 +0800

    Minor removal of unused libraries import in InventoryControllerTest.java.

--- backend/src/test/java/com/retailpulse/controller/InventoryControllerTest.java
+++ backend/src/test/java/com/retailpulse/controller/InventoryControllerTest.java
@@ -5,10 +5,8 @@ import com.retailpulse.service.InventoryService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
