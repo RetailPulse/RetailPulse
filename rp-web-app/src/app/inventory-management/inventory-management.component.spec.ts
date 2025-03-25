cf773cb8531d4f636be4b21449687d60a3e692fb
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2025-03-25 23:34:03 +0800

    fix: fixing the httpClient

--- rp-web-app/src/app/inventory-management/inventory-management.component.spec.ts
+++ rp-web-app/src/app/inventory-management/inventory-management.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InventoryManagementComponent } from './inventory-management.component';
+import { HttpClientModule } from '@angular/common/http';
+import {BusinessEntityService} from '../business-entity-management/business-entity.service';
+
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    imports: [HttpClientModule], // Add this
+    providers: [BusinessEntityService] // Ensure this service is included
+  }).compileComponents();
+});
 
 describe('ProductManagementComponent', () => {
   let component: InventoryManagementComponent;
